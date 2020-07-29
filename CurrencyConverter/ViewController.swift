ad85877d7ad75b1dec17658d3444e1bcb8405de0
JimA <jm.allan@ntlworld.com>
2020-07-29 09:24:21 +0100

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Jim Allan on 29/07/2020.
+//  Copyright © 2020 Jim Allan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
